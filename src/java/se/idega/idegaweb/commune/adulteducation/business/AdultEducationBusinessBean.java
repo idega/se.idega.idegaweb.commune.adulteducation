eff05a3947813ac604c28769e512880eca31c08d
Thorhallur Helgason <laddi@idega.com>
2005-07-04 13:11:02 +0000

    Bug fixes.

--- src/java/se/idega/idegaweb/commune/adulteducation/business/AdultEducationBusinessBean.java
+++ src/java/se/idega/idegaweb/commune/adulteducation/business/AdultEducationBusinessBean.java
@@ -1,5 +1,5 @@
 /*
- * $Id: AdultEducationBusinessBean.java,v 1.37 2005/07/04 10:16:08 laddi Exp $ Created on
+ * $Id: AdultEducationBusinessBean.java,v 1.38 2005/07/04 13:11:02 laddi Exp $ Created on
  * 27.4.2005
  * 
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
@@ -80,10 +80,10 @@ import com.idega.util.IWTimestamp;
 /**
  * A collection of business methods associated with the Adult education block.
  * 
- * Last modified: $Date: 2005/07/04 10:16:08 $ by $Author: laddi $
+ * Last modified: $Date: 2005/07/04 13:11:02 $ by $Author: laddi $
  * 
  * @author <a href="mailto:laddi@idega.com">laddi</a>
- * @version $Revision: 1.37 $
+ * @version $Revision: 1.38 $
  */
 public class AdultEducationBusinessBean extends CaseBusinessBean implements AdultEducationBusiness {
 
@@ -1305,6 +1305,7 @@ public class AdultEducationBusinessBean extends CaseBusinessBean implements Adul
 	public void removePlacement(SchoolClassMember student, AdultEducationChoice choice, User performer) throws RemoveException {
 		SchoolClass group = student.getSchoolClass();
 		School school = group.getSchool();
+		User user = student.getStudent();
 		AdultEducationCourse course = null;
 		SchoolStudyPath path = null;
 		try {
@@ -1325,7 +1326,7 @@ public class AdultEducationBusinessBean extends CaseBusinessBean implements Adul
 		}
 		
 		try {
-			getMessageBusiness().createUserMessage(student.getStudent(), subject, MessageFormat.format(body, arguments));
+			getMessageBusiness().createUserMessage(user, subject, MessageFormat.format(body, arguments));
 		}
 		catch (RemoteException re) {
 			throw new IBORuntimeException(re);
